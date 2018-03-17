2ffe0436b0e0b5328815af01d392d97dad1cb854
yykhomenko <yykhomenko@gmail.com>
2018-03-17 14:46:21 +0200

    changed init spark

--- src/main/scala/org/test/analyzer/conf/SparkConfig.scala
+++ src/main/scala/org/test/analyzer/conf/SparkConfig.scala
@@ -6,16 +6,13 @@ import org.apache.spark.{SparkConf, SparkContext}
 object SparkConfig {
 
   lazy val conf = new SparkConf().setAppName("fine-food-analyzer").setMaster("local[*]")
-  lazy val sc = new SparkContext(conf)
 
-  lazy val sparkSession = SparkSession.builder
-    .config(conf)
-    .appName("fine-food-analyzer")
-    .getOrCreate()
+  lazy val sc = SparkContext.getOrCreate(conf)
+  lazy val ss = SparkSession.builder.config(conf).getOrCreate()
 
-  def dfFrom(path: String) = sparkSession.read.option("header", "true").csv(path)
+  def dfFrom(path: String) = ss.read.option("header", "true").csv(path)
   def rddFrom(path: String) = { sc.setLogLevel("ERROR"); sc.textFile(path) }
 
   def stopContext() = sc.stop()
-  def stopSession() = sparkSession.stop()
+  def stopSession() = ss.stop()
 }
